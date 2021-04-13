bddcdbf8ba57d373d2cf3c8c275c0908fb61b686
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-13 14:29:19 -0400

    wrote migration for cars table

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,1 +1,15 @@
-// DO YOUR MAGIC
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", table=>{
+      table.increments() //primary key
+    table.text("vin", 32).unique().notNullable()
+    table.text("make", 128).notNullable()
+    table.text("model").notNullable()
+    table.integer("mileage").notNullable()
+    table.text("title", 128)
+    table.text("transmission", 128)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars")
+};
