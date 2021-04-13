09c77e150d848a0be577efe6e077dfbbe4249feb
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-13 15:10:14 -0400

    Build main structure of server and router

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,23 @@
-// DO YOUR MAGIC
+// takes routes from requests
+// needs express, needs to be taught how to read json, needs knex, exports, and 
+const express = require('express')
+const db = require()
+//import middleware
+const router = express.Router() // build an instance of express
+router.use(express.json())
+
+router.get("/", (req, res, next) => {
+  next()
+})
+
+router.get("/:id", (req, res, next) => {
+  next()
+})
+router.post("/", (req, res, next) => {
+  next()
+})
+
+
+
+
+module.exports = router // Gives the outside world access to the router we've defined here
--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,9 +1,9 @@
 exports.up = function(knex) {
   return knex.schema.createTable("cars", table=>{
-      table.increments() //primary key
-    table.text("vin", 32).unique().notNullable()
-    table.text("make", 128).notNullable()
-    table.text("model").notNullable()
+    table.increments() //primary key
+    table.text("vin", 17).unique().notNullable()
+    table.text("make", 32).notNullable()
+    table.text("model", 32).notNullable()
     table.integer("mileage").notNullable()
     table.text("title", 128)
     table.text("transmission", 128)
