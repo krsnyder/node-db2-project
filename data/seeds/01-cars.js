4e6f507818e11ed557d05f46f0d01414ea6fca8f
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-13 17:17:26 -0400

    Built seed data

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
-// STRETCH
+exports.seed = function (knex) {
+  return knex('cars').truncate()
+    .then(function () {
+      return knex('cars').insert([
+        {
+          vin: 'N4AC7FE42B78G35HJ',
+          make: "Toyota",
+          model: "Tacoma",
+          mileage: 46136,
+          title: "lease",
+          transmission: "automatic"
+        },
+        {
+          vin: 'L4LS98CC7345AZ0L1',
+          make: "Subaru",
+          model: "Crosstrek",
+          mileage: 23098,
+          title: "lease",
+          transmission: "manual"
+        },
+        {
+          vin: 'D93MB7SQ6DLBGJ523',
+          make: "Geo",
+          model: "Tracker",
+          mileage: 198463,
+          title: "clean",
+          transmission: "manual"
+        }
+    ])
+  })
+}
