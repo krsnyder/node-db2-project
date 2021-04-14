a91cb7608c9748e255f37a08b7aa839039d9135e
krsnyder <kirkrobertsnyder@gmail.com>
2021-04-13 21:19:50 -0400

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -5,6 +5,7 @@ const getAll = () => {
 };
 
 const getById = (id) => {
+  //originally destructured id from arguments but received the error SQLITE_ERROR: no such column: id
   return db("cars").where("car_id", id).first();
 };
 
